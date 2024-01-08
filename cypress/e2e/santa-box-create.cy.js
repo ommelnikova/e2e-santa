262f57efbd261a568e024db755aae6497ee6e14b
KrisGromova <levkindom@gmail.com>
2024-01-08 18:18:53 +0300

    add box delete_hw6.1.2

--- cypress/e2e/santa-box-create.cy.js
+++ cypress/e2e/santa-box-create.cy.js
@@ -24,29 +24,36 @@ describe("user can create a box and run it", () => {
   let maxAmount = 50;
   let currency = "Евро";
   let inviteLink;
+  let newBoxId;
 
-  it("user logins and create a box", () => {
+  it.only("user logins and create a box", () => {
     cy.visit("/login");
     cy.login(users.userAutor.email, users.userAutor.password);
     cy.wait(5000); // добавляем задержку в 5 секунд для ожидания загрузки страницы
     cy.contains("Создать коробку").should("be.visible"); // проверяем, что элемент отображается на странице
     cy.contains("Создать коробку").click();
-    cy.wait(10000);
+    cy.wait(5000);
     cy.get(boxPage.boxNameField)
       .type(newBoxName)
       .should("have.value", newBoxName);
     cy.get(boxPage.boxNameField)
       .invoke("val", newBoxName) // сохраняем значение в переменную
       .as("newBoxName"); // сохраняем значение в контекст Cypress
+    cy.get(boxPage.boxIdField)
+      .invoke("val")
+      .then((value) => {
+        newBoxId = value;
+      });
     cy.get(generalElements.arrowRight).click();
-    cy.wait(10000);
+    cy.wait(5000);
     cy.get(boxPage.sixthIcon).click();
     cy.get(generalElements.arrowRight).click();
-    cy.wait(10000);
+    cy.wait(3000);
     cy.get(boxPage.giftPriceToggle).check({ force: true });
     cy.get(boxPage.maxAnount).type(maxAmount);
     cy.get(boxPage.currency).select(currency);
     cy.get(generalElements.arrowRight).click();
+    cy.wait(3000);
     cy.get(boxPage.checkAddress).should("have.text", "Почтовый адрес");
     cy.get(boxPage.checkPhoneNumber).should("have.text", "Номер телефона");
     cy.get(generalElements.arrowRight).click();
@@ -58,7 +65,7 @@ describe("user can create a box and run it", () => {
         expect(text).to.include("Моя карточка");
         expect(text).to.include("Подопечный");
       });
-      });
+  });
 
   it("add participants", () => {
     cy.get(generalElements.addParticipantsButton).click();
@@ -114,37 +121,51 @@ describe("user can create a box and run it", () => {
         ).type(users[slicedKeys[i]].email);
       }
     });
-     cy.get(invitePage.inviteUserButton).click();
-     cy.wait(10000);
-     cy.get(".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner"
+    cy.get(invitePage.inviteUserButton).click();
+    cy.wait(10000);
+    cy.get(
+      ".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner"
     ).click();
-    cy.get(".layout-1__header-wrapper-fixed > .layout-1__header-secondary__menu > .header-secondary-menu > .organizer-menu > .organizer-menu__wrapper > :nth-child(2)"
+    cy.get(
+      ".layout-1__header-wrapper-fixed > .layout-1__header-secondary__menu > .header-secondary-menu > .organizer-menu > .organizer-menu__wrapper > :nth-child(2)"
     ).click({ force: true });
-     cy.get(".btn-main").click();
-     cy.get(".santa-modal_content_buttons > .btn-main").click();
-     cy.get(".picture-notice__title").should(
-       "have.text",
-       "Жеребьевка проведена"
-     );
-     cy.clearCookies();
-   });
+    cy.get(".btn-main").click();
+    cy.get(".santa-modal_content_buttons > .btn-main").click();
+    cy.get(".picture-notice__title").should(
+      "have.text",
+      "Жеребьевка проведена"
+    );
+    cy.clearCookies();
+  });
 
-  // after("delete box", () => {
-  //   cy.visit("/login");
-  //   cy.login(users.userAutor.email, users.userAutor.password);
-  //   cy.contains("Коробки").click({ force: true });
-  //   // cy.get(
-  //   //   "#root > div.layout-1 > section.layout-1__header-wrapper-fixed > header > section > div > div > a:nth-child(1)"
-  //   // ).click();
-  //   cy.contains(newBoxName, { timeout: 1000 }).click({ force: true });
-  //   // cy.get(":nth-child(1) > a.base--clickable > .user-card").first().click();
-  //   cy.get(
-  //     ".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner"
-  //   ).click();
-  //   cy.contains("Архивация и удаление").click({ force: true });
-  //   cy.get(":nth-child(2) > .form-page-group__main > .frm-wrapper > .frm").type(
-  //     "Удалить коробку"
-  //   );
-  //   cy.get(".btn-service").click();
-  // });
+  after("delete box", () => {
+    cy.visit("/login");
+    cy.login(users.userAutor.email, users.userAutor.password);
+    cy.request({
+      method: "Delete",
+      url: "https://santa-secret.ru/box/" + newBoxId,
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+    });
+  });
 });
+
+// after("delete box", () => {
+// cy.visit("/login");
+// cy.login(users.userAutor.email, users.userAutor.password);
+//     cy.contains("Коробки").click({ force: true });
+//     // cy.get(
+//     //   "#root > div.layout-1 > section.layout-1__header-wrapper-fixed > header > section > div > div > a:nth-child(1)"
+//     // ).click();
+//     cy.contains(`${newBoxName}`, { timeout: 1000 }).click({ force: true });
+//     //cy.get(":nth-child(1) > a.base--clickable > .user-card").first().click();
+//     cy.get(
+//       ".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner"
+//     ).click();
+//     cy.contains("Архивация и удаление").click({ force: true });
+//     cy.get(":nth-child(2) > .form-page-group__main > .frm-wrapper > .frm").type(
+//       "Удалить коробку"
+//     );
+//     cy.get(".btn-service").click();
+//   });
+// });
